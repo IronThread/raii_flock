371ffc24b6a6d3c55ae455a68ef64b0ce0f06dc2
Dang <zeroed32@protonmail.com>
2021-08-18 20:38:02 -0300

    change to receive references instead

--- src/lib.rs
+++ src/lib.rs
@@ -7,44 +7,45 @@ use ::{
             fs::File,
             io::{self, SeekFrom, prelude::*},
             ops::{Deref, DerefMut},
+            thread::panicking,
         },
 };
 
 /// Wrapper over a file that calls [`FileExt::unlock`] at [dropping][`Drop`].
 #[derive(Debug)]
-pub struct FileLock(pub File);
+pub struct FileLock<'a>(pub &'a File);
 
-impl FileLock {
+impl<'a> FileLock<'a> {
     /// Creates a `Self` instance calling [`FileExt::try_lock_shared`] on `f` and returning any
     /// error that could have caused.
-    pub fn try_wrap_shared(f: File) -> io::Result<Self> {
+    pub fn try_wrap_shared(f: &'a File) -> io::Result<Self> {
         f.try_lock_shared()?;
         Ok(Self(f))
     }
 
     /// Creates a `Self` instance calling [`FileExt::lock_shared`] on `f` and returning any
     /// error that could have caused.
-    pub fn wrap_shared(f: File) -> io::Result<Self> {
+    pub fn wrap_shared(f: &'a File) -> io::Result<Self> {
         f.lock_shared()?;
         Ok(Self(f))
     }
 
     /// Creates a `Self` instance calling [`FileExt::try_lock_exclusive`] on `f` and returning any
     /// error that could have caused.
-    pub fn try_wrap_exclusive(f: File) -> io::Result<Self> {
+    pub fn try_wrap_exclusive(f: &'a File) -> io::Result<Self> {
         f.try_lock_exclusive()?;
         Ok(Self(f))
     }
 
     /// Creates a `Self` instance calling [`FileExt::lock_exclusive`] on `f` and returning any
     /// error that could have caused.
-    pub fn wrap_exclusive(f: File) -> io::Result<Self> {
+    pub fn wrap_exclusive(f: &'a File) -> io::Result<Self> {
         f.lock_exclusive()?;
         Ok(Self(f))
     }
 }
 
-impl Write for FileLock {
+impl<'a> Write for FileLock<'a> {
     #[inline(always)]
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         self.0.write(buf)
@@ -56,48 +57,22 @@ impl Write for FileLock {
     }
 }
 
-impl<'a> Write for &'a FileLock {
-    #[inline(always)]
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        (&self.0).write(buf)
-    }
-
-    #[inline(always)]
-    fn flush(&mut self) -> io::Result<()> {
-        (&self.0).flush()
-    }
-}
-
-impl Read for FileLock {
+impl<'a> Read for FileLock<'a> {
     #[inline(always)]
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         self.0.read(buf)
     }
 }
 
-impl<'a> Read for &'a FileLock {
-    #[inline(always)]
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        (&self.0).read(buf)
-    }
-}
-
-impl Seek for FileLock {
+impl<'a> Seek for FileLock<'a> {
     #[inline(always)]
     fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
         self.0.seek(pos)
     }
 }
 
-impl<'a> Seek for &'a FileLock {
-    #[inline(always)]
-    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
-        (&self.0).seek(pos)
-    }
-}
-
-impl Deref for FileLock {
-    type Target = File;
+impl<'a> Deref for FileLock<'a> {
+    type Target = &'a File;
 
     #[inline(always)]
     fn deref(&self) -> &Self::Target {
@@ -105,17 +80,21 @@ impl Deref for FileLock {
     }
 }
 
-impl DerefMut for FileLock {
+impl<'a> DerefMut for FileLock<'a> {
     #[inline(always)]
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
     }
 }
 
-impl Drop for FileLock {
+impl<'a> Drop for FileLock<'a> {
     fn drop(&mut self) {
         if let Err(e) = self.0.unlock() {
-            eprintln!("error file lock: {}", e)
+            if panicking() {
+                eprintln!("error unlocking file lock: {}", e)
+            } else {
+                panic!("error unlocking file lock: {}", e)
+            }
         }
     }
 }
